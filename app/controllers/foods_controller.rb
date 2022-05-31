dd1675fa62a72d10d99516d945913afa4a617950
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-05-31 10:40:53 +0500

    :lipstick: Added UI for food form with basic styling

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,27 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.foods.new(name: food_params[:name],
+                                   measurement_unit: food_params[:measurement_unit],
+                                   price: food_params[:price])
+    if @food.save
+      flash[:success] = 'Food is successfully created'
+      redirect_to foods_path
+    else
+      flash[:error] = 'Invalid Entry'
+      redirect_to new_food_path
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'public_recipes#index'
-  resources :foods, only: [:index]
+  resources :foods, only: [:index, :new, :create]
   resources :recipes, only: [:index, :show]
   resources :public_recipes, only: [:index]
   resources :general_shopping_list, only: [:index]
