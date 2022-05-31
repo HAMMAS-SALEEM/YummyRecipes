0e36fab90230f622755ffb936296d9411903af61
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-01 01:34:00 +0500

    :art: completely configured devise for application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'public_recipes#index'
   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
