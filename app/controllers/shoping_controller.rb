8c539811792c7bdc5bca579def262b6e61723cc6
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-31 19:13:42 +0200

    Fix linters errrors

--- app/controllers/shoping_controller.rb
+++ app/controllers/shoping_controller.rb
@@ -3,14 +3,15 @@ class ShopingController < ApplicationController
     @shop = []
     @total = 0
 
-    results = Food.join(:receipe_foods).select(:foods, 'foods.*',  'sum(recipe_foods.quantity) as rf_q').joins(:inventory_foods).select('inventory_foods.quantity as if_q').group('foods.id, inventory_foods.quantity')
+    results = Food.join(:receipe_foods).select(:foods, 'foods.*',
+                                               'sum(recipe_foods.quantity) as rf_q').joins(:inventory_foods).select('inventory_foods.quantity as if_q').group('foods.id, inventory_foods.quantity')
 
-    results.each do |result| 
+    results.each do |result|
       next unless result.rf_q > result.if_q
-  
+
       quantity = result.ref - result.if_q
-      @shop << { foo: result, quantity: , price: quantity * result.price }
+      @shop << { foo: result, quantity:, price: quantity * result.price }
       @total += quantity * result.price
     end
   end
-end 
+end
--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,4 +1,3 @@
 class ShopingController < ApplicationController
-  def index
-  end
-end 
+  def index; end
+end
