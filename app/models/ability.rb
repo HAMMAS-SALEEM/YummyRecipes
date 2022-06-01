082f59ff9650be7d8fb4f67ba9c07229b6a68935
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-02 02:41:27 +0500

    :bug: fixed public recipe page total cost issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Recipe, pulic: true
+    can :read, Recipe, user_id: user.id
 
     can :manage, Recipe, user_id: user.id unless user.nil?
     can :manage, Food, user_id: user.id unless user.nil?
