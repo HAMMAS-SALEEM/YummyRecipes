8821cfe7f79607ec13834f9fa999ba44288a8b41
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-01 11:52:19 +0200

    Add validations on the Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,11 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods, dependent: :destroy
 
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :preparation_time, presence: true, length: { maximum: 10 }
+  validates :cooking_time, presence: true, length: { maximum: 10 }
+  validates :description, presence: true
+
   def total_price
     total = 0
     recipe_foods.each do |recipe_food|
