b6ecadecef8c8327b1223873d8d3760f93c43e9b
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-06-02 09:15:54 +0500

    :ok_hand: added styling for navbar

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -12,7 +12,7 @@ class GeneralShoppingListsController < ApplicationController
     current_user.recipes.includes(:recipe_foods).each do |recipe|
       recipe.recipe_foods.includes(:food).each do |recipe_food|
         total_recipe_foods << { name: recipe_food.food.name, quantity: recipe_food.quantity,
-                                price: recipe_food.food.price, measurement_unit: recipe_food.food.measurement_unit}
+                                price: recipe_food.food.price, measurement_unit: recipe_food.food.measurement_unit }
       end
     end
     total_recipe_foods
